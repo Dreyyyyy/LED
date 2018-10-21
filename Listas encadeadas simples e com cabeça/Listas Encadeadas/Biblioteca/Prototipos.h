2e3e0aea56670121d85274f05a7989ba0b89020e
Dreyyyyy <andreydias@alunos.utfpr.edu.br>
2018-10-21 12:45:18 -0300

    Trabalho para o dia 24/10/18

---
+++ Listas encadeadas simples e com cabeça/Listas Encadeadas/Biblioteca/Prototipos.h
@@ -0,0 +1,21 @@
+/*
+* Faça uma biblioteca para as definições
+* abaixo especificadas.
+*/
+typedef int TipoChave;
+typedef int TipoValor;
+struct TipoListaSimples {
+TipoChave chave;
+TipoValor valorQualquer;
+struct TipoListaSimples *prox;
+};typedef struct TipoListaSimples TipoListaSimples;
+
+TipoListaSimples *insereInicioListaSimples(TipoListaSimples **prim, TipoChave chave, TipoValor valor);
+
+void atualizaValor(TipoListaSimples *prim, TipoValor novoValor);
+
+void removePrimeiroNo(TipoListaSimples **prim);
+
+TipoListaSimples *pesquisaNo(TipoListaSimples *prim, TipoChave chave);
+
+TipoListaSimples * insereFimListaSimples(TipoListaSimples **prim, TipoChave chave);
